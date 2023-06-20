bd20e7609560d6e552dfc918fd4611057586afd7
Alena Aleksenko <106320275+AleksenkoA@users.noreply.github.com>
2023-06-20 14:01:56 +0300

    map

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.header__menu');
+let navToggle = document.querySelector('.header__menu-toggle');
+
+navMain.classList.remove('header__menu--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header__menu--closed')) {
+    navMain.classList.remove('header__menu--closed');
+    navMain.classList.add('header__menu--opened');
+  } else {
+    navMain.classList.add('header__menu--closed');
+    navMain.classList.remove('header__menu--opened');
+  }
+});
